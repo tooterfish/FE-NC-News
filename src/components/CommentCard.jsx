eb6b2a3a76ef47080bbd5a00fe35d8f5b299cf2d
Harry Fleming <email@harryfleming.net>
2023-04-18 13:34:03 +0100

    add votes/post date to comment card, add some styling to make seperation of article body and comments clearer

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,6 +1,8 @@
 export default function CommentCard({comment}) {
   return <li className="comment-card">
     <h4>{comment.author}</h4>
+    <span>posted on {comment.created_at}</span>
+    <span>votes {comment.votes}</span>
     <p>{comment.body}</p>
   </li>
 }
