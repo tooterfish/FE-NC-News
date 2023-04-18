7a60417272692cec7d3dab5f9eef8cc689aa834b
Harry Fleming <email@harryfleming.net>
2023-04-18 16:33:57 +0100

    made posted date for comments more human readable

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,8 +1,9 @@
 export default function CommentCard({comment}) {
+  const date = new Date(comment.created_at)
   return <li className="comment-card">
     <h4>{comment.author}</h4>
-    <span>posted on {comment.created_at}</span>
-    <span>votes {comment.votes}</span>
+    <span>posted on {date.toLocaleDateString()} at {date.toLocaleTimeString()}</span>
     <p>{comment.body}</p>
+    <span>votes: {comment.votes}</span>
   </li>
 }
