f1bb1a0427227fb40ebe088b6505ec1c70399446
Harry Fleming <email@harryfleming.net>
2023-04-19 09:58:22 +0100

    add all to topicnav

--- src/components/TopicNav.jsx
+++ src/components/TopicNav.jsx
@@ -4,6 +4,9 @@ export default function TopicNav({topics}) {
   const topicArray = Object.keys(topics)
   return <div className="topic-nav">
         <ul>
+        <li key="all">
+          <Link to="/articles">all</Link>
+        </li>
         {
         topicArray.map((topic) => {
           return <li key={topic}>
