8659625b2a3a7ea2fcbdb3f3f6390fa9b98b29ad
Harry Fleming <email@harryfleming.net>
2023-04-19 09:13:38 +0100

    added loading element to usercard

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,8 +4,14 @@ import { useContext } from 'react'
 export default function UserCard() {
   const { user } = useContext(UserContext)
 
+  const isUser = () => {
+    if (user.username) return false
+    return true
+  }
+
   return <div className="user-card">
+    { isUser() ? <h3>Loading...</h3> : <>
     <img src={user.avatar_url} alt={`${user.name}'s avatar`}/>
-    <h4>{user.username}</h4>
+    <h4>{user.username}</h4> </>}
   </div>
 }
