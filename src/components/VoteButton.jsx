e262ef7f625f483ff9bbb1a0a9023df4445ea60f
Harry Fleming <email@harryfleming.net>
2023-04-20 16:24:03 +0100

    add flag so user can only vote once per session

--- src/components/VoteButton.jsx
+++ src/components/VoteButton.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 export default function VoteButton({votes, id, voteFunc}) {
 
   const [currentVotes, setVotes] = useState()
+  const [voted, setVoted] = useState(false)
 
   useEffect(() => {
     setVotes(votes)
@@ -10,14 +11,16 @@ export default function VoteButton({votes, id, voteFunc}) {
 
   function handleVote() {
     setVotes(votes => votes + 1)
+    setVoted(true)
     voteFunc(id)
     .then(() => {
     })
     .catch(() => {
       setVotes(votes => votes - 1)
+      setVoted(false)
     })
   }
-  return <button onClick={handleVote} className="vote-button">
+  return <button disabled={voted} onClick={handleVote} className="vote-button">
           <div className="sym">
             &#11088;
           </div>
