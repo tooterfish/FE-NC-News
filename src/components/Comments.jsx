bff5b4a7c742bf443e7804ced91386c553503669
Harry Fleming <email@harryfleming.net>
2023-04-21 14:44:12 +0100

    add small buffer to clientHeight to ensure infinite scroll triggers on some devices where it wasn't previously

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -4,7 +4,7 @@ import { fetchComments } from '../api'
 import CommentForm from './CommentForm'
 import CommentCard from './CommentCard'
 
-export default function Comments({ articleId, totalComments }) {
+export default function Comments({ articleId }) {
   const [commentList, setCommentList] = useState([])
   const [currentPage, setPage] = useState(1)
   const [loadingMore, setLoadingMore] = useState(false)
@@ -12,13 +12,14 @@ export default function Comments({ articleId, totalComments }) {
 
   useEffect(() => {
     if (commentList.length === 0) loadMoreComments()
-    
+
     function onScroll() {
       const scrollTop = document.documentElement.scrollTop
       const scrollHeight = document.documentElement.scrollHeight
       const clientHeight = document.documentElement.clientHeight
-  
-      if ((scrollTop + clientHeight >= scrollHeight)) loadMoreComments()
+
+      if (scrollTop + clientHeight + 100 >= scrollHeight) {
+        loadMoreComments()}
     }
 
     function loadMoreComments() {
@@ -42,8 +43,6 @@ export default function Comments({ articleId, totalComments }) {
     return () => { window.removeEventListener('scroll', onScroll)}
   }, [currentPage, articleId, commentList])
 
-
-
   return <div className="comments">
     <CommentForm articleId={articleId} setCommentList={setCommentList}/>
     <ul>
@@ -53,6 +52,6 @@ export default function Comments({ articleId, totalComments }) {
     })
     }
     </ul>
-    {(loadingMore && moreCommentsFound) ? <div className="loader"></div> : <></>}
+    {(loadingMore && moreCommentsFound) ? <div className="loader"></div> : <div></div>}
   </div>
 }
